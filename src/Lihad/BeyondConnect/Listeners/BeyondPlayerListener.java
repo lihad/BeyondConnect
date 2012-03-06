fb59d3d6e5fb8983dd29df5ad8a9f71b0fbfc537
lihad <kylecubed@comcast.net>
2012-03-05 21:52:43 -0800

    Added collision protection

--- src/Lihad/BeyondConnect/Listeners/BeyondPlayerListener.java
+++ src/Lihad/BeyondConnect/Listeners/BeyondPlayerListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.Listener;
 
 import Lihad.BeyondConnect.BeyondConnect;
 import Lihad.BeyondConnect.JSON.JSONConnection;
+import Lihad.BeyondConnect.Util.BeyondTimerTask;
 
 public class BeyondPlayerListener implements Listener {
 	public static BeyondConnect plugin;
@@ -20,7 +21,7 @@ public class BeyondPlayerListener implements Listener {
 			if(event.getClickedBlock().getType() == Material.SPONGE && BeyondConnect.handler.inGroup(event.getPlayer().getWorld().getName(), event.getPlayer().getName(), "Drifter")){
 				System.out.println("-------------------CONNECTION-DEBUG-----------");
 				System.out.println("Sponge hit by player: "+event.getPlayer());
-				if(JSONConnection.stream.toString().toLowerCase().contains(("\""+event.getPlayer().getName()+"\"").toLowerCase())){
+				if(BeyondTimerTask.acquired && JSONConnection.stream.toString().toLowerCase().contains(("\""+event.getPlayer().getName()+"\"").toLowerCase())){
 					System.out.println("Player "+event.getPlayer().getName()+" was found.  Promoted");
 					plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), "pex promote "+event.getPlayer().getName());
 					event.getPlayer().sendMessage(ChatColor.BLUE.toString()+"Congrats!  You've been accepted!!! You can leave this area now");
@@ -31,6 +32,7 @@ public class BeyondPlayerListener implements Listener {
 					event.getPlayer().sendMessage(ChatColor.BLUE.toString()+"----------------------------------------------");
 					event.getPlayer().sendMessage(ChatColor.BLUE.toString()+"You Haven't Registered Yet! You Must Do So First");
 					event.getPlayer().sendMessage(ChatColor.BLUE.toString()+"Please Go To BeyondMinecraft.net and Register!");
+					event.getPlayer().sendMessage(ChatColor.BLUE.toString()+"If you did register, wait 30sec and try again!");
 					event.getPlayer().sendMessage(ChatColor.BLUE.toString()+"----------------------------------------------");
 				}
 				System.out.println("----------------------------------------------");
